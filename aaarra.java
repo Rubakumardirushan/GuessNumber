a67d57b0188012d384d3640b0214c0647ec64ebe
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-06-27 12:52:21 +0530

    Update aaarra.java

--- aaarra.java
+++ aaarra.java
@@ -1,7 +1,7 @@
 public class aaarra {
     public static void main(String args[]){
-        String name[]; // initiate array
-        name= new String[3]; // define array;
+        String name[]; // array is define
+        name= new String[3]; // array is initialize
         name[0]="dirushan";
         name[1]="msd";
         name[2]="virat";
