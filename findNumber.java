99c3bf43423a118d4ed51ba8e528427ffba7b918
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-06-27 10:43:03 +0530

    first commit

---
+++ findNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class findNumber{
+    public static void main(String args[]){
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter your name");
+       String name= input.nextLine();
+       System.out.println("your name is "+name);
+    }
+}
