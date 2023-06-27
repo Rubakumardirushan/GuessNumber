6689f8f2ac1dcb022bcce72d11b6df8c05b35bb9
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2023-06-27 12:45:09 +0530

    simple array

---
+++ aaarra.java
@@ -0,0 +1,12 @@
+public class aaarra {
+    public static void main(String args[]){
+        String name[]; // initiate array
+        name= new String[3]; // define array;
+        name[0]="dirushan";
+        name[1]="msd";
+        name[2]="virat";
+        for(String z: name){
+            System.out.println(z);
+        }
+    }
+}
